c37262f6d03545a153943ec143a1dc27be343e82
Diluvies <nevermore@postech.ac.kr>
2023-11-30 19:58:18 +0900

    minor error fix

--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,7 +1,7 @@
 #ifndef VM_SWAP_H
 #define VM_SWAP_H
 
-typedef unsigned __int64 size_t;
+typedef unsigned int size_t;
 void swap_init (void);
 void swap_in (struct spt_entry *, void *);
 size_t swap_out (void *);
